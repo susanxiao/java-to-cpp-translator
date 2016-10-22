f20cd3691043dbd30c22c336a848555487e94acc
Susan Xiao <sx398@nyu.edu>
2016-10-22 00:49:24 -0400

    Added test cases for each input

--- src/test/java/edu/nyu/oop/AstTraversalTest.java
+++ src/test/java/edu/nyu/oop/AstTraversalTest.java
@@ -1,6 +1,7 @@
 package edu.nyu.oop;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import xtc.tree.GNode;
 
@@ -10,20 +11,158 @@ import static org.junit.Assert.assertEquals;
  * Created by Garrett on 10/12/16.
  */
 public class AstTraversalTest {
-    private static GNode node = null;
 
     @BeforeClass
     public static void beforeClass() {
-        System.out.println("Executing AstBuilder");
-        node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test001/Test001.java");
+        System.out.println("Executing AstTraversal");
+        // node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test005/Test005.java");
         // XtcTestUtils.prettyPrintAst(node);
     }
 
     @Test
-    public void testInput001() {
+    public void test000() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test000/Test000.java");
         AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
         AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
 
+    @Test
+    public void test001() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test001/Test001.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
 
+    @Test
+    public void test002() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test002/Test002.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Test
+    public void test003() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test003/Test003.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Test
+    public void test004() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test004/Test004.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Test
+    public void test005() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test005/Test005.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Test
+    public void test006() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test006/Test006.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Test
+    public void test007() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test007/Test007.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test008() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test008/Test008.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test009() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test009/Test009.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test010() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test010/Test010.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test011() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test011/Test011.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test012() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test012/Test012.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test013() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test013/Test013.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test014() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test014/Test014.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test015() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test015/Test015.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test016() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test016/Test016.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test017() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test017/Test017.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test018() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test018/Test018.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test019() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test019/Test019.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+    }
+
+    @Ignore @Test
+    public void test020() {
+        GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test020/Test020.java");
+        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
+        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
     }
 }
