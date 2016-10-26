fff38af715241d23787a2b4b81b8f361345d102d
Susan Xiao <sx398@nyu.edu>
2016-10-26 14:14:41 -0400

    General implementation of mutator

--- src/main/java/edu/nyu/oop/AstMutator.java
+++ src/main/java/edu/nyu/oop/AstMutator.java
@@ -1,9 +1,78 @@
 package edu.nyu.oop;
 
+import org.slf4j.Logger;
+import xtc.tree.Node;
 import xtc.tree.Visitor;
+import xtc.util.Runtime;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static java.lang.System.out;
 
 /**
  * Created by susan on 10/26/16.
  */
 public class AstMutator extends Visitor {
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
+    private Runtime runtime;
+    boolean debug = false;
+    private AstMutator.AstMutatorSummary summary = new AstMutator.AstMutatorSummary();
+
+    public AstMutator(Runtime runtime) {
+        this.runtime = runtime;
+    }
+
+    public AstMutator.AstMutatorSummary getMutator(Node n) {
+
+        super.dispatch(n);
+
+        if (debug) {
+            out.println("\n");
+            out.println("DEBUGGING IS ON");
+            for (ClassImplementation c : summary.classes.values()) {
+                out.println(c.toString());
+            }
+        }
+
+        return summary;
+    }
+
+    static class AstMutatorSummary {
+
+        HashMap<String, ClassImplementation> classes = new HashMap<>();
+        ArrayList<String> classNames = new ArrayList<>();
+
+        ArrayList<String> currentPackages = new ArrayList<>();
+
+        ClassImplementation currentClass;
+        MethodImplementation currentMethod;
+        ConstructorImplementation currentConstructor;
+
+        // operators ?
+        String operators = "=+-*/";
+
+        public void addClass(ClassImplementation superClass, String name, String modifier) {
+            ClassImplementation c = new ClassImplementation(superClass, name, modifier);
+            c.packages.addAll(currentPackages);
+
+            classes.put(name, c);
+            classNames.add(name);
+            currentClass = c;
+        }
+
+        public void addMethod(MethodImplementation m) {
+            currentClass.addMethod(m);
+            currentMethod = m;
+        }
+
+        public void addConstructor(ConstructorImplementation constructor) {
+            currentClass.addConstructor(constructor);
+            currentConstructor = constructor;
+        }
+
+        public ClassImplementation findClass(String name) {
+            return classes.get(name);
+        }
+    }
 }
--- src/test/java/edu/nyu/oop/AstMutatorTest.java
+++ src/test/java/edu/nyu/oop/AstMutatorTest.java
@@ -5,184 +5,183 @@ import org.junit.Ignore;
 import org.junit.Test;
 import xtc.tree.GNode;
 
-import static org.junit.Assert.assertEquals;
-
 /**
  * Created by Garrett on 10/12/16.
  */
-public class AstTraversalTest {
+public class AstMutatorTest {
 
     @BeforeClass
     public static void beforeClass() {
-        System.out.println("Executing AstTraversal");
+        System.out.println("Executing AstMutator");
         // node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test005/Test005.java");
         // XtcTestUtils.prettyPrintAst(node);
     }
 
+    @Test
     public void test000() {
         System.out.println("*********************** Test 000 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test000/Test000.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Test
     public void test001() {
         System.out.println("*********************** Test 001 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test001/Test001.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test002() {
         System.out.println("*********************** Test 002 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test002/Test002.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test003() {
         System.out.println("*********************** Test 003 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test003/Test003.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test004() {
         System.out.println("*********************** Test 004 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test004/Test004.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test005() {
         System.out.println("*********************** Test 005 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test005/Test005.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test006() {
         System.out.println("*********************** Test 006 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test006/Test006.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test007() {
         System.out.println("*********************** Test 007 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test007/Test007.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test008() {
         System.out.println("*********************** Test 008 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test008/Test008.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test009() {
         System.out.println("*********************** Test 009 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test009/Test009.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test010() {
         System.out.println("*********************** Test 010 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test010/Test010.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test011() {
         System.out.println("*********************** Test 011 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test011/Test011.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test012() {
         System.out.println("*********************** Test 012 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test012/Test012.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test013() {
         System.out.println("*********************** Test 013 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test013/Test013.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test014() {
         System.out.println("*********************** Test 014 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test014/Test014.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test015() {
         System.out.println("*********************** Test 015 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test015/Test015.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test016() {
         System.out.println("*********************** Test 016 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test016/Test016.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test017() {
         System.out.println("*********************** Test 017 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test017/Test017.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test018() {
         System.out.println("*********************** Test 018 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test018/Test018.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test019() {
         System.out.println("*********************** Test 019 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test019/Test019.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 
     @Ignore @Test
     public void test020() {
         System.out.println("*********************** Test 020 ***********************");
         GNode node = (GNode) XtcTestUtils.loadTestFile("./src/test/java/inputs/test020/Test020.java");
-        AstTraversal visitor = new AstTraversal(XtcTestUtils.newRuntime());
-        AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        AstMutator visitor = new AstMutator(XtcTestUtils.newRuntime());
+        AstMutator.AstMutatorSummary summary = visitor.getMutator(node);
     }
 }
