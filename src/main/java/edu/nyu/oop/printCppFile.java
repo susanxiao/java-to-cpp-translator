2c37fb0545978a6fb04b828e6c5a7dac6a525246
gar26 <grc266@nyu.edu>
2016-10-27 20:35:18 -0400

    started main printing implementation

--- src/main/java/edu/nyu/oop/printCppFile.java
+++ src/main/java/edu/nyu/oop/printCppFile.java
@@ -6,6 +6,9 @@ import xtc.tree.Node;
 import xtc.tree.Visitor;
 import xtc.util.Runtime;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -25,8 +28,8 @@ public class printCppFile extends Visitor {
     StringBuilder cppImplementation = new StringBuilder();
 
     // visitXXX methods
-    public void visitClassDeclaration(GNode n){
-        if(n.getString(1).contains("Test")){
+    public void visitClassDeclaration(GNode n) {
+        if (n.getString(1).contains("Test")) {
             return;
         }
         summary.currentClassName = n.getString(1);
@@ -39,32 +42,32 @@ public class printCppFile extends Visitor {
         visitClassBody((GNode) n.getNode(5));
     }
 
-    public void visitClassBody(GNode n){
-        for(Object methods : n){
+    public void visitClassBody(GNode n) {
+        for (Object methods : n) {
             GNode currentMethod = (GNode) methods;
-            if(currentMethod.getName().equals("MethodDeclaration")){
+            if (currentMethod.getName().equals("MethodDeclaration")) {
                 visitMethodDeclaration(currentMethod);
             }
         }
     }
 
-    public void visitMethodDeclaration(GNode n){
+    public void visitMethodDeclaration(GNode n) {
         String type = n.getNode(2).getNode(0).getString(0);
         String methodName = n.getString(3);
         // check if the method contains parameters
-        if(n.getNode(4).size() > 0){
+        if (n.getNode(4).size() > 0) {
 
         }
         Node methodBlock = n.getNode(7);
-        for(Object o : methodBlock){
+        for (Object o : methodBlock) {
             GNode currentNode = (GNode) o;
-            if(currentNode.getName().equals("ReturnStatement")){
+            if (currentNode.getName().equals("ReturnStatement")) {
                 visitReturnStatement(currentNode);
             }
         }
     }
 
-    public void visitReturnStatement(GNode n){
+    public void visitReturnStatement(GNode n) {
         String returnStatement = "";
 
     }
@@ -86,9 +89,10 @@ public class printCppFile extends Visitor {
 
     static class printCppFileSummary {
         String currentClassName;
+        String filePrinted;
     }
 
-    public printCppFileSummary getSummary(GNode n){
+    public printCppFileSummary getSummary(GNode n) {
         StringBuilder s1 = new StringBuilder();
         s1.append("\n------------------\n\n");
         s1.append("#include \"output.h\"\n");
@@ -98,8 +102,8 @@ public class printCppFile extends Visitor {
         Node packageNode = n.getNode(0).getNode(1);
         String namespace = "";
         int namespaceCounter = 0;
-        for(Object packageName : packageNode){
-            for(int i = 0; i < namespaceCounter; i++){
+        for (Object packageName : packageNode) {
+            for (int i = 0; i < namespaceCounter; i++) {
                 namespace += "\t";
             }
             namespace += packageName.toString() + "{\n";
@@ -109,9 +113,9 @@ public class printCppFile extends Visitor {
 
 
         // visit the class declarations
-        for(Object o : n){
+        for (Object o : n) {
             GNode currentClass = (GNode) o;
-            if(currentClass.getName().equals("ClassDeclaration")){
+            if (currentClass.getName().equals("ClassDeclaration")) {
                 visitClassDeclaration(currentClass);
             }
         }
@@ -130,7 +134,10 @@ public class printCppFile extends Visitor {
         }
         s1.append(closeNamespace);
         s1.append("\n------------------\n\n");
-        out.println(s1.toString());
+        //out.println(s1.toString());
+
+        summary.filePrinted = s1.toString();
+
         return summary;
     }
 
@@ -141,14 +148,50 @@ public class printCppFile extends Visitor {
         AstTraversal visitorTraversal = new AstTraversal(LoadFileImplementations.newRuntime());
         AstTraversal.AstTraversalSummary summaryTraversal = visitorTraversal.getTraversal(node);
 
-        // get the mutated tree
-        AstMutator visitor1 = new AstMutator(LoadFileImplementations.newRuntime());
-        AstMutator.AstMutatorSummary summary = visitor1.getMutator(node);
-
-        // get the summary of the cpp implementations
-        printCppFile visitor = new printCppFile(LoadFileImplementations.newRuntime(), summaryTraversal);
-        printCppFileSummary summaryCpp = visitor.getSummary(node);
         //LoadFileImplementations.prettyPrintAst(node);
+        for (int i = 0; i < 2; i++) {
+            String test = "./src/test/java/inputs/";
+            String test1 = "";
+            String test2 = "";
+            if (i < 10) {
+                test1 = "test00" + i;
+                test2 = "Test00" + i;
+            } else {
+                test1 = "test0" + i;
+                test2 = "Test0" + i;
+            }
+            test += test1;
+            test += "/" + test2 + ".java";
+            try {
+                PrintWriter printerOutput;
+
+                File output;
+
+                // get the mutated tree
+                AstMutator visitor1 = new AstMutator(LoadFileImplementations.newRuntime());
+                AstMutator.AstMutatorSummary summary = visitor1.getMutator(node);
+
+                // get the summary of the cpp implementations
+                printCppFile visitor = new printCppFile(LoadFileImplementations.newRuntime(), summaryTraversal);
+                printCppFileSummary summaryCpp = visitor.getSummary(node);
+
+                String outputCppFile = "";
+                outputCppFile += summaryCpp.filePrinted;
+
+                output = new File("testOutputs/printCppFile", test2);
+                output.createNewFile();
+
+                printerOutput = new PrintWriter(output);
+                printerOutput.println(outputCppFile);
+                printerOutput.flush();
+                printerOutput.close();
+                out.println("output " + i + " printed\n");
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
 
     }
--- src/main/java/edu/nyu/oop/printMainFile.java
+++ src/main/java/edu/nyu/oop/printMainFile.java
@@ -1,7 +1,14 @@
 package edu.nyu.oop;
 
+import org.slf4j.Logger;
 import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+import xtc.util.Runtime;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,49 +17,120 @@ import static java.lang.System.out;
 /**
  * Created by Garrett on 10/21/16.
  */
-public class printMainFile {
+public class printMainFile extends Visitor {
+
+
+    private printMainFile.printMainFileSummary summary = new printMainFile.printMainFileSummary();
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
+
+    private Runtime runtime;
+    private AstTraversal.AstTraversalSummary summaryTraversal;
+
+    StringBuilder mainImplementation = new StringBuilder();
+
+    // visitXXX methods
+
+    // visitMethod
+    public void visit(Node n) {
+        for (Object o : n) {
+            if (o instanceof Node) {
+                dispatch((Node) o);
+            }
+        }
+    }
+
+
+    public printMainFile(Runtime runtime, AstTraversal.AstTraversalSummary summaryTraversal) {
+        this.runtime = runtime;
+        this.summaryTraversal = summaryTraversal;
+    }
+
+    static class printMainFileSummary {
+        String currentClassName;
+        String filePrinted;
+    }
+
+    public printMainFile.printMainFileSummary getSummary(GNode n) {
+        StringBuilder s1 = new StringBuilder();
+
+        s1.append("\n------------------\n\n");
+
+        s1.append("#include <iostream>\n#include <sstream>\n");
+        s1.append("#include \"java_lang.h\"\n\n");
+        s1.append("#include \"output.h\"\n");
+        s1.append("\n" +
+                "using namespace java::lang;\n" +
+                "using namespace inputs::javalang;\n" +
+                "using namespace std;\n\n");
+        s1.append("int main(void)\n{\n\n");
+
+
+
+        // append the return statement
+        s1.append("\treturn 0;\n");
+        s1.append("}");
+
+        s1.append("\n\n------------------\n\n");
+
+        out.println(s1.toString());
+
+        summary.filePrinted = s1.toString();
+
+        return summary;
+    }
+
+
     public static void main(String[] args) {
+        GNode node = (GNode) LoadFileImplementations.loadTestFile("./src/test/java/inputs/test001/Test001.java");
 
-        /**
+        // get the summary traversal (class implementations)
+        AstTraversal visitorTraversal = new AstTraversal(LoadFileImplementations.newRuntime());
+        AstTraversal.AstTraversalSummary summaryTraversal = visitorTraversal.getTraversal(node);
 
-         GNode node = (GNode) LoadFileImplementations.loadTestFile("./src/test/java/inputs/test001/Test001.java");
-         AstTraversal visitor = new AstTraversal(LoadFileImplementations.newRuntime());
-         AstTraversal.AstTraversalSummary summary = visitor.getTraversal(node);
+        //LoadFileImplementations.prettyPrintAst(node);
+        for (int i = 0; i < 2; i++) {
+            String test = "./src/test/java/inputs/";
+            String test1 = "";
+            String test2 = "";
+            if (i < 10) {
+                test1 = "test00" + i;
+                test2 = "Test00" + i;
+            } else {
+                test1 = "test0" + i;
+                test2 = "Test0" + i;
+            }
+            test += test1;
+            test += "/" + test2 + ".java";
+            try {
+                PrintWriter printerMain;
 
-         HashMap<String, ClassImplementation> classes = summary.classes;
+                File main;
 
-         ArrayList<String> keys = summary.classNames;
-         ArrayList<String> classNames = keys;
+                // get the mutated tree
+                AstMutator visitor1 = new AstMutator(LoadFileImplementations.newRuntime());
+                AstMutator.AstMutatorSummary summary = visitor1.getMutator(node);
 
-         ArrayList<String> classesImplementing = new ArrayList<>();
+                // get the summary of the cpp implementations
+                printMainFile visitor = new printMainFile(LoadFileImplementations.newRuntime(), summaryTraversal);
+                printMainFile.printMainFileSummary summaryMain = visitor.getSummary(node);
 
-         for (Object o : classNames) {
-         String className = o.toString();
-         ClassImplementation currentClass = classes.get(className);
-         if (currentClass.modifier == "public") {
-         classesImplementing.add(className);
-         }
-         }
+                String mainFile = "";
+                mainFile += summaryMain.filePrinted;
 
-         StringBuilder s = new StringBuilder();
+                main = new File("testOutputs/mainFileOutputs", test2);
+                main.createNewFile();
 
-         ClassImplementation mainClass = classes.get(classesImplementing.get(0));
+                printerMain = new PrintWriter(main);
+                printerMain.println(mainFile);
+                printerMain.flush();
+                printerMain.close();
+                out.println("main " + i + " printed\n");
 
-         s.append("int main(void)\n{\n");
-         for(MethodImplementation currentMethod : mainClass.methods){
 
-         for(MethodStatement currentStatement : currentMethod.implementation){
-         s.append("  " + currentStatement.toCpp() + "\n");
-         }
-
-         }
-
-         s.append("  return 0;\n");
-         s.append("}\n");
-
-         out.println(s.toString());
-
-         **/
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
