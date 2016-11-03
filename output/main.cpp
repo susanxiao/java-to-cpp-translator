c14ac25aea4b01281efcdab45e61a73dcc0e66f1
Susan Xiao <sx398@nyu.edu>
2016-11-02 20:09:46 -0400

    Test run test000

--- output/main.cpp
+++ output/main.cpp
@@ -9,38 +9,15 @@
 
 using namespace java::lang;
 using namespace std;
-using namespace inputs::test010;
+using namespace inputs::test000;
 
 int main(void)
 {
 
-	A a = new __A();
-
-	a->__vptr->setA(a, new __String("A"));
-
-	B1 b1 = new __B1();
-
-	b1->__vptr->setA(b1, new __String("B1"));
-
-	B2 b2 = new __B2();
-
-	b2->__vptr->setA(b2, new __String("B2"));
-
-	C c = new __C();
-
-	c->__vptr->setA(c, new __String("C"));
-
-	a->__vptr->printOther(a);
-
-	a->__vptr->printOther(b1);
-
-	a->__vptr->printOther(b2);
-
-	a->__vptr->printOther(c);
+	cout << "Hello."  << endl;
 
 	return 0;
 }
 
 //------------------
-
 
