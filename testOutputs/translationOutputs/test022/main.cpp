67866fe43205e013b58444b2580a57713c4399f8
JinkyungLim <jl4819@nyu.edu>
2016-11-28 01:48:09 -0500

    fixed test22. Used struct Array in java_lang to get command line arguments. edited main(void) to main(int argc, char ** argv), and make argv more like args in java

--- testOutputs/translationOutputs/test022/main.cpp
+++ testOutputs/translationOutputs/test022/main.cpp
@@ -11,14 +11,21 @@ using namespace java::lang;
 using namespace std;
 using namespace inputs::test022;
 
-int main (int argc, char ** args) 
+int main (int argc, char ** argv) 
 {
 
+	//get command line arguments. convert between args(java) and argv(c++). for test22, 23...
+	__rt::Array<java::lang::String> args(argc-1);
+	for(int a=1; a<argc;a++){
+		java::lang::String argument = new __String(argv[a]);
+		args.__data[a-1] = argument;
+	}
+
 	//for statement
-	for(int i=0; i<args.length ; i++)
+	for(int32_t i=0; i<args.length ; i++)
 
 	{
-		cout << args[i] << endl;
+		cout << args.__data[i]->data << endl;
 
 	}
 
