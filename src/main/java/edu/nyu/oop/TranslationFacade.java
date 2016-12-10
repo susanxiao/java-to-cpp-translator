ba625dc99ca89f917a2afb193d0cc42b2b1574e4
gar26 <grc266@nyu.edu>
2016-12-09 20:58:32 -0500

    TranslationFacade cleanup

--- src/main/java/edu/nyu/oop/TranslationFacade.java
+++ src/main/java/edu/nyu/oop/TranslationFacade.java
@@ -17,10 +17,8 @@ import static java.lang.System.out;
  */
 class HeaderComponent {
 
-    PrintHeaderFile.headerFileSummary headerFileSummary = null;
-    AstTraversal.AstTraversalSummary summaryTraversal = null;
-
-
+    private PrintHeaderFile.headerFileSummary headerFileSummary = null;
+    private AstTraversal.AstTraversalSummary summaryTraversal = null;
     private String filePath = null;
     private String printPath = null;
     private GNode root = null;
@@ -31,6 +29,10 @@ class HeaderComponent {
         this.root = root;
     }
 
+    public AstTraversal.AstTraversalSummary getSummaryTraversal() {
+        return this.summaryTraversal;
+    }
+
     public void updateHeader() {
         AstTraversal visitor1 = new AstTraversal(newRuntime());
         // PHASE 1: obtain the traversal summary
@@ -38,12 +40,12 @@ class HeaderComponent {
         // PHASE 2: construct the c++ header Ast
         GNode headerNode = HeaderAst.getHeaderAst(summaryTraversal).parent;
         // PHASE 3: create the header code
-        headerFileSummary = new PrintHeaderFile(newRuntime(), summaryTraversal).getSummary(headerNode);
+        this.headerFileSummary = new PrintHeaderFile(newRuntime(), summaryTraversal).getSummary(headerNode);
     }
 
     public void printHeader() {
-        String headerPath = printPath + "output.h";
-        File header = new File(headerPath);
+        File header = new File(printPath + "output.h");
+        header.getParentFile().mkdirs();
 
         try {
             FileWriter printHeader = new FileWriter(header);
@@ -67,14 +69,14 @@ class CppComponent {
     private String printPath = null;
     private GNode root = null;
     private PrintCppFile.cppFileSummary cppSummary = null;
-    AstTraversal.AstTraversalSummary summaryTraversal = null;
+    private AstTraversal.AstTraversalSummary summaryTraversal = null;
 
     public CppComponent(String printPath, GNode root) {
         this.printPath = printPath;
         this.root = root;
     }
 
-    public GNode getRoot(){
+    public GNode getRoot() {
         return this.root;
     }
 
@@ -82,16 +84,24 @@ class CppComponent {
         this.summaryTraversal = summaryTraversal;
     }
 
+    public AstTraversal.AstTraversalSummary getSummaryTraversal() {
+        return this.summaryTraversal;
+    }
+
+    public PrintCppFile.cppFileSummary getCppSummary() {
+        return this.cppSummary;
+    }
+
+
     public void updateCpp() {
         // PHASE 4: mutate the traversed tree
         new AstMutator(newRuntime()).mutate(root);
         // PHASE 5: create the cpp code
-        cppSummary = new PrintCppFile(newRuntime(), summaryTraversal).getSummary(root);
+        this.cppSummary = new PrintCppFile(newRuntime(), summaryTraversal).getSummary(root);
     }
 
     public void printCpp() {
         File output = new File(printPath + "output.cpp");
-        output.getParentFile().mkdirs();
         try {
             FileWriter printOutput = new FileWriter(output);
             printOutput.write(cppSummary.code.toString());
@@ -127,12 +137,12 @@ class MainComponent {
         this.summaryTraversal = summaryTraversal;
     }
 
-    public void setRoot(GNode root){
+    public void setRoot(GNode root) {
         this.root = root;
     }
 
     public void updateMain() {
-        mainSummary = new PrintMainFile(newRuntime(), this.summaryTraversal).getSummary(this.root);
+        this.mainSummary = new PrintMainFile(newRuntime(), summaryTraversal).getSummary(root);
     }
 
     public void printMain() {
@@ -148,7 +158,7 @@ class MainComponent {
         }
     }
 
-    public void execute(){
+    public void execute() {
         this.updateMain();
         this.printMain();
     }
@@ -169,9 +179,9 @@ public class TranslationFacade {
 
     public void translate() {
         headerController.execute();
-        cppController.setTraversal(headerController.summaryTraversal);
+        cppController.setTraversal(headerController.getSummaryTraversal());
         cppController.execute();
-        mainController.setTraversal(cppController.summaryTraversal);
+        mainController.setTraversal(cppController.getSummaryTraversal());
         mainController.setRoot(cppController.getRoot());
         mainController.execute();
     }
